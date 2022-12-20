03eb94407b326e124feddd63b54b574fd19e35e9
bdamirul0 <bdamirul0@gmail.com>
2022-12-20 23:43:55 +0600

    uses of printf and n

---
+++ helloN.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    printf("My name is Amirul Islam Limon.\n");
+    printf("I live in Dhaka, Bangladesh!.\n");
+    printf("Where are you from ?\n");
+    return 0;
+}
---
+++ printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("My name is Amirul Islam.\n I was a college student");
+    printf("In 2023, I will be a college student.\n");
+    printf("So, Pray for me. I hope 2023 will be a good year for me.");
+
+    printf("\n%d\n", 5 + 5);
+    printf("%d\n", 5 - 10);
+    printf("%d\n", 5 * 10);
+    printf("%d\n", 50 / 10);
+
+    return 0;
+}
