a25548fee60fd1f0fbed982f025473e4531ba41f
bdamirul0 <bdamirul0@gmail.com>
2022-12-27 10:01:03 +0600

    learning about data type is done!!

---
+++ dataType.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    float myFloat = 9;
+    printf("%f\n",myFloat);
+
+    float sum = (float) 5 / 2;
+    printf("%f",sum);
+
+    // printf("Hello World!\n\n");
+
+    return 0;
+}
