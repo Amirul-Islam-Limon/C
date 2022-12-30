4b751850a3cc6cd579eaa657b6284600fbee90f6
bdamirul0 <bdamirul0@gmail.com>
2022-12-30 08:54:14 +0600

    leant about bool type data in c

---
+++ booleans.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+
+    printf("%d\n",5 < 9);
+
+    int myAge = 19;
+    int votingAge = 18;
+
+    printf("%d\n",myAge >= votingAge); 
+
+    bool programmingIsFun = true;
+    bool imIOld = false;
+    printf("%d, %d",programmingIsFun,imIOld);
+
+    return 0;
+}
