919e4e690edb3f6d9b21e91eec38f78cc31de87c
Amirul Islam <bdamirul0@gmail.com>
2023-01-03 09:53:53 +0600

    today was a switch daygit add .

---
+++ basicCalculator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+
+    double num1;
+    double num2;
+    printf("Please Enter Your First Number: ");
+    scanf("%lf", &num1);
+    printf("Please Enter Second Number: ");
+    scanf("%lf", &num2);
+    printf("Your result is : %f\n", num1 + num2);
+
+    return 0;
+}
---
+++ switch.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(){
+
+    int num;
+    printf("Please Enter Your Faverite day number : ");
+    scanf("%d",&num);
+
+    switch (num)
+    {
+    case 1:
+        printf("Saturday");
+        break;
+    case 2:
+    printf("Sunday");
+        break;
+    case 3:
+        printf("Monday");
+        break;
+    case 4:
+        printf("Tuesday");
+        break;
+    case 5:
+        printf("Wednday");
+        break;
+    case 6:
+        printf("Thursday!");
+        break;
+    case 7:
+        printf("friday");
+        break;    
+    default:
+        printf("Sorry , Not Found!!");
+    }
+
+
+
+    return 0;
+}
