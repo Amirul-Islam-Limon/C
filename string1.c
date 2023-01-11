c4bf8d494c88409d8879801b065593e9f393fb66
Amirul Islam <bdamirul0@gmail.com>
2023-01-11 09:41:56 +0600

    interecting bacslash escape character

---
+++ string1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+    // backslash escape character.
+
+    printf("Hello, every one. It\'s me\n");
+    printf("There is a proverb say\'s that, \" Time and tide wait for none\"\n");
+    printf("I will eat tea\\ coffee\\cold water\n");
+
+    return 0;
+}
